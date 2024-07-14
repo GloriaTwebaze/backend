51db2d437c934f0d807ebf6cb42513f5ba2b154b
GloriaTwebaze <twebazegloria909@gmail.com>
2024-07-14 20:08:52 +0300

    created end points

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    taskname = models.CharField(max_length=255, null=True, blank=True)
+    description = models.CharField(max_length=255,null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.taskname
